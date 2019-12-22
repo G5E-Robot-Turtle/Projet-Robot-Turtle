a1933badd39eb7891f023ae35bb349cea471d7e1
ABCD1-1 <55496947+ABCD1-1@users.noreply.github.com>
2019-12-22 21:34:29 +0100

    .

--- Robot_Turtle/src/com/company/Grid.java
+++ Robot_Turtle/src/com/company/Grid.java
@@ -3,29 +3,71 @@ package com.company;
 import cell.*;
 
 public class Grid {
-    private Cell[][] grid = new Cell[8][8];
+    private int line =8;
+    private int column=8;
+    private Cell[][] grid = new Cell[line][column];
 
 
     public Grid() {                               //plateau 8*8
         initGrid();
     }
 
+
     public Cell[][] getGrid() {
         return grid;
     }
 
-    public void initGrid() {  //à compléter
+    public void initGrid() {  //  pour 2 joueur
         Player player1 = new Player();             //test
-        Empty empty1 = new Empty();
+        Player player2 = new Player("Turtle","red",Direction.SOUTH,2);
+        Jewel jewel = new Jewel();
+        Empty empty = new Empty();
 
-        System.out.println(grid[0][0]);
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                grid[i][j] = empty1;
+                grid[i][j] = empty;
             }
         }
-        grid[0][0] = player1;
+        grid[0][1] = player1;
+        grid[0][5] = player2;
+        grid[7][3] = jewel;
         displayGrid(8, 8);
+    }
+
+    public void initGrid(int nbJoueur) {  //  pour 2 joueur
+        Player player1 = new Player();
+        Player player2 = new Player("Turtle","red",Direction.SOUTH,2);
+        Jewel jewel = new Jewel();
+        Empty empty = new Empty();
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                grid[i][j] = empty;                 //une soixantaine de empty n'est pas nécessaire ?
+            }
+        }
+        switch (nbJoueur){
+            case 2:
+                grid[0][1] = player1;
+                grid[0][5] = player2;
+                grid[7][3] = jewel;
+                //placer des murs en colonne 7 ligne 0 à 7
+                displayGrid(8, 8);
+                break;
+            case 3:
+                Player player3 = new Player("Turtle","purple",Direction.SOUTH,3);
+                //créer jewel 2 et 3
+                grid[0][0] = player1;
+                grid[0][3] = player2;
+                //faire pour player3
+                grid[7][0] = jewel;
+                //faire pour les deux autres jewel
+                //placer des murs en colonne 7 ligne 0 à 7
+                break;
+            case 4:
+                //remplir
+                break;
+            default:
+                System.out.println("Nombre de joueurs invalide !");
+        }
     }
 
 
