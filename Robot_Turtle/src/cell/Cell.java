cccf4f83ac616b8e652e5e5b84d5a1083ea7c463
BParayre <55878014+BParayre@users.noreply.github.com>
2020-01-10 18:51:36 +0100

    Update Cell.java

--- Robot_Turtle/src/cell/Cell.java
+++ Robot_Turtle/src/cell/Cell.java
@@ -9,6 +9,7 @@ public abstract class Cell {
     protected Empty empty;
     protected String name = "I'm a cell";
     public static TreeMap<Integer, Jewel> positionJewels = new TreeMap<>();
+    public static TreeMap<Integer, Block> positionWalls = new TreeMap<>();
     protected boolean check = false;
     public static TreeMap<Integer, Player> positionPlayers = new TreeMap<>();  //<position, Player> pour savoir  s'il y a un joueur à une telle position
 
@@ -31,5 +32,20 @@ public abstract class Cell {
     public int convertPositionToInt(int line, int column) {  // convertit un tableau de position en nombre entier ([2][1] devient 21), utilise pour gérer les collisions
         int caseNum = 10 * line + column;
         return caseNum;
+    }
+        public boolean isAvailable(int pos) {
+        if (positionWalls.containsKey(pos)) {
+            return false;
+        } else {
+            if (positionJewels.containsKey(pos)) {
+                return false;
+            } else {
+                if (positionPlayers.containsKey(pos)) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
     }
 }
