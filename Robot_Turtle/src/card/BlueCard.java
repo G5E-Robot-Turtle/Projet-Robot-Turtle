46f5e4095277e30c92feda9a511b96e0a796aac7
ABCD1-1 <55496947+ABCD1-1@users.noreply.github.com>
2020-01-21 14:21:22 +0100

    Update BlueCard.java

--- Robot_Turtle/src/card/BlueCard.java
+++ Robot_Turtle/src/card/BlueCard.java
@@ -5,17 +5,7 @@ public class BlueCard implements Card {
     private String description = "Make the turtle move one square forward.";
 
     public BlueCard() {
-    }
 
-    public BlueCard(String name, String description) {
-        this.name = name;   //mettre getName() si y'a un bug
-        this.description = description;
-    }
-
-    @Override
-    public void playCard() {
-//        super.playCard();           //cette ligne a été écrite toute seule   ,
-        // PS:  super() c'est pour faire appel aux variables, méthodes de la classe mère dans nos constructeurs
     }
 
     @Override
@@ -23,8 +13,4 @@ public class BlueCard implements Card {
         return this.name;
     }
 
-    @Override
-    public String getDescription() {
-        return this.description;
-    }
 }
